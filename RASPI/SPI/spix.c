5060ee649e4815eea3a6ff1a40ea2c69847066c3
chipyards <noullet@chipyards.com>
2020-02-20 18:13:32 +0100

    RASPI/SPI/spix.c teste avec STM32F103 github.com/chipyards/F1_keil_ats 779d889

--- RASPI/SPI/spix.c
+++ RASPI/SPI/spix.c
@@ -6,23 +6,22 @@ gcc -Wall -o spix spix.c libbcm2835.a
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void usage()
 {
 printf("Ce programme pour Raspberry peut :\n");
-printf("  envoyer un message de 5 octets sur une liaison SPI dont le raspberry est maitre\n");
-printf("  recevoir 5 octets en parallele\n");
+printf("  envoyer un message texte ascii sur une liaison SPI dont le raspberry est maitre\n");
+printf("  recevoir et afficher le meme nombre de bytes en parallele\n");
 printf("options :\n");
-printf("  -v <v1> <v2> <v3> : 3 octets a envoyer au milieu du message\n"); 
 printf("  -r <t> : repetition a intervalles de t secondes\n");
 exit(1);
 }
 
-#include <ctype.h>	// juste pour isdigit()
 #include <unistd.h>	// juste pour sleep (que je croyais deprecated...)
-
-
 #include "bcm2835.h"
+
+/* deprecated produit des glitches sur NSS entre les bytes 
 int spi_dial( unsigned char * txdata, unsigned char * rxdata, int N )
 {
 int i;
@@ -45,6 +44,7 @@ bcm2835_spi_end();
 bcm2835_close();
 return 0;
 }
+*/
 
 int spin_dial( unsigned char * txdata, unsigned char * rxdata, int N )
 {
@@ -66,59 +66,58 @@ return 0;
 }
 
 
-#define DIAL_SIZE 5
+#define DIAL_SIZE 256
 
 int main( int argc, char ** argv )
 {
-int val[3];
+unsigned char txbuf[DIAL_SIZE];
+unsigned char rxbuf[DIAL_SIZE];
+int msg_len = 0;
 
 // options
-int opt_repeat = -1; val[0] = -1;
+int opt_repeat = -1; txbuf[0] = 0;
 
 int iarg = 1;
 while	( iarg < argc )
 	{
 	if	( argv[iarg][0] != '-' )
-		usage();
-	switch	( argv[iarg][1] )
-		{
-		case 'r' : if ( argc < (iarg+2) ) usage();
-			   if ( !isdigit( argv[iarg+1][0] ) ) usage();
-			   opt_repeat = atoi( argv[iarg+1] );
-			   ++iarg; break;
-		case 'v' : if ( argc < (iarg+4) ) usage();
-			   if ( !isdigit( argv[iarg+1][0] ) ) usage();
-			   if ( !isdigit( argv[iarg+2][0] ) ) usage();
-			   if ( !isdigit( argv[iarg+3][0] ) ) usage();
-			   val[0] = atoi( argv[iarg+1] );
-			   val[1] = atoi( argv[iarg+2] );
-			   val[2] = atoi( argv[iarg+3] );
-			   iarg += 3; break;
-		default : usage();
+		{			// le ';' est vu comme delimiteur par le STM32
+		snprintf( (char *)txbuf, sizeof(txbuf), "%s\n;", argv[iarg] );
+		txbuf[sizeof(txbuf)-1] = 0;
+		}
+	else	{
+		switch	( argv[iarg][1] )
+			{
+			case 'r' : if	( argc < (iarg+2) ) usage();
+				   opt_repeat = atoi( argv[iarg++] );
+				   if	( opt_repeat <= 0 ) usage();
+				   break;
+			default : usage();
+			}
 		}
 	++iarg;
 	}
+msg_len = strlen( (char *)txbuf );
+if	( msg_len <= 0 )
+	usage();
 
 // action eventuellement repetee indefiniment
 do	{
 	int i, retval;
-	unsigned char txdata[DIAL_SIZE] = { 0xCA, 0xFE, 0xBB, 0xD0, 0xD0 };
-	unsigned char rxdata[DIAL_SIZE];
-	if	( val[0] >= 0 )
-		{
-		txdata[1] = val[0];
-		txdata[2] = val[1];
-		txdata[3] = val[2];
-		}
-	retval = spin_dial( txdata, rxdata, DIAL_SIZE );
+	retval = spin_dial( txbuf, rxbuf, msg_len );
 	if	( retval == 0 )
 		{
+		char c;
 		printf("rx : ");
-		for	( i = 0; i < DIAL_SIZE; ++i )
-			printf("%02x ", rxdata[i] );
+		for	( i = 0; i < msg_len; ++i )
+			{
+			c = rxbuf[i];
+			if	( c != 0 )
+				printf("%c", c );
+			}
 		printf("\n");
 		}
-	else	{ printf("spi_dial failed %d\n", retval ); break; }
+	else	{ printf("spin_dial failed %d\n", retval ); break; }
 	if	( opt_repeat >= 0 )
 		printf("repetition automatique (periode %d s), Ctrl-C pour stopper\n", opt_repeat );
 	if	( opt_repeat > 0 )
